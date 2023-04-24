f400dae6ce7add1b5445aa3f93f8daefa4c6d17b
vandervel <72418762+vandervel@users.noreply.github.com>
2023-04-24 11:54:12 -0700

    added

--- Easy/duplicate_emails.sql
+++ Easy/duplicate_emails.sql
@@ -4,3 +4,10 @@ select email as 'Email'
 from Person
 group by Email
 having count(*) > 1
+
+
+/* alternative query */
+
+select distinct p.email as 'Email'
+from Person as p 
+where Email in(select email from person where email = p.email and id != p.id)
