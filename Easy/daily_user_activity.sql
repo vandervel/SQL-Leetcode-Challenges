bad215b52c6509be9b95e99036d2f4aca049196b
vandervel <72418762+vandervel@users.noreply.github.com>
2023-04-28 10:24:32 -0700

    added

---
+++ Easy/daily_user_activity.sql
@@ -0,0 +1,7 @@
+/* get daily user activity for a 30 day period ending on 27 July 2019 */
+
+
+select activity_date as 'day', count(distinct user_id) as 'active_users'
+from Activity
+where activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date
