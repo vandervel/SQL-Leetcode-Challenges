ba68c5ca033772432a9ad3f2f306daeefea847dc
vandervel <72418762+vandervel@users.noreply.github.com>
2023-05-01 11:04:21 -0700

    added

---
+++ Easy/reformat_table.sql
@@ -0,0 +1,9 @@
+/* reformat table such that each month has its own column for revenue for each id */
+
+select id, Jan as Jan_Revenue,Feb as Feb_Revenue ,Mar as Mar_Revenue,Apr as Apr_Revenue
+,May as May_Revenue,Jun as Jun_Revenue,Jul as Jul_Revenue,Aug as Aug_Revenue,Sep as Sep_Revenue,
+Oct as Oct_Revenue,  Nov as Nov_Revenue,Dec as Dec_Revenue from department
+Pivot
+(
+sum(revenue) for Month in (Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec)
+)As PT
